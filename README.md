2b8b715fc24c0bfa257e26d73b88de2150ce75f8
melosomelo <mateusmelornrn@gmail.com>
2024-06-26 22:13:07 -0300

    aaaaaaaaaaaaaa

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Computer Architecture and Organization (cao)
 
-This repository is a collection of [SystemC](https://systemc.org/) implementations and explanations
-of hardware components that I wrote while taking a course in Computer Organization/Architecture
-at [UFRN](https://en.wikipedia.org/wiki/Federal_University_of_Rio_Grande_do_Norte).
-
 To run the project, compile it with `make` and then run with `./proc`. The program accepts two optional command
 line arguments: `--data` and `--instruction`. These are files that should hold, respectively, the contents
 from the data memory and the instruction memory. You can use the ones in the `examples` folder. The command
--- noc/src/index.ts
+++ noc/src/index.ts
@@ -1,1 +1,144 @@
-console.log("eai");
+import fs from "fs";
+import path from "path";
+
+interface Coordinate {
+  x: number;
+  y: number;
+}
+interface Config {
+  blockedCells: Array<Coordinate>;
+  packets: Array<{
+    origin: Coordinate;
+    destination: Coordinate;
+    payload: number;
+  }>;
+}
+
+enum ActionType {
+  Move = "move",
+  Wait = "wait",
+}
+
+enum Direction {
+  North = "north",
+  South = "south",
+  East = "east",
+  West = "west",
+}
+interface MoveAction {
+  type: ActionType.Move;
+  direction: Direction;
+}
+
+interface WaitAction {
+  type: ActionType.Wait;
+  packetCulprit: number;
+}
+
+type Action = WaitAction | MoveAction;
+
+class Packet {
+  history: Array<Action> = [];
+  position: Coordinate;
+  constructor(
+    public origin: Coordinate,
+    public destination: Coordinate,
+    public data: number,
+    public id: number
+  ) {
+    this.position = { ...origin };
+  }
+
+  hasReachedDestination() {
+    return (
+      this.position.x === this.destination.x &&
+      this.position.y === this.destination.y
+    );
+  }
+}
+
+function main(config: Config) {
+  const blockedCells = new Set<string>(
+    config.blockedCells.map((cell) => `${cell.x},${cell.y}`)
+  );
+  const packets = config.packets.map(
+    (packet, i) =>
+      new Packet(packet.origin, packet.destination, packet.payload, i)
+  );
+  while (packets.some((packet) => !packet.hasReachedDestination())) {
+    for (
+      let i = 0;
+      i < packets.filter((packet) => !packet.hasReachedDestination()).length;
+      i++
+    ) {
+      const packet = packets[i];
+      const nextDirection = calculateNextDirection(packet);
+      const nextPosition = move(packet.position, nextDirection);
+      const { hasPacketInIt, packetCulprit } = cellHasPacketInIt(
+        nextPosition,
+        packets
+      );
+      if (hasPacketInIt)
+        packet.history.push({
+          type: ActionType.Wait,
+          packetCulprit: packetCulprit!,
+        });
+      else {
+        packet.history.push({
+          type: ActionType.Move,
+          direction: nextDirection,
+        });
+        packet.position = nextPosition;
+      }
+    }
+  }
+
+  for (let packet of packets) {
+    console.log(packet.history);
+  }
+}
+
+function calculateNextDirection(packet: Packet): Direction {
+  if (packet.position.x < packet.destination.x) {
+    return Direction.East;
+  }
+  if (packet.position.x > packet.destination.x) {
+    return Direction.West;
+  }
+  if (packet.position.y < packet.destination.y) {
+    return Direction.South;
+  }
+  return Direction.North;
+}
+
+function cellHasPacketInIt(cell: Coordinate, packets: Array<Packet>) {
+  const packetCulprit = packets.find(
+    (packet) =>
+      packet.position.x === cell.x &&
+      packet.position.y === cell.y &&
+      !packet.hasReachedDestination()
+  );
+  return {
+    hasPacketInIt: packetCulprit !== undefined,
+    packetCulprit: packetCulprit?.id,
+  };
+}
+
+function move(position: Coordinate, direction: Direction): Coordinate {
+  if (direction === Direction.East) {
+    return { x: position.x + 1, y: position.y };
+  }
+  if (direction === Direction.West) {
+    return { x: position.x - 1, y: position.y };
+  }
+  if (direction === Direction.South) {
+    return { x: position.x, y: position.y + 1 };
+  }
+  return { x: position.x, y: position.y - 1 };
+}
+
+const config = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "config.json")).toString()
+) as Config;
+
+main(config);
