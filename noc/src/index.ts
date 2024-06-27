79b165ff33c3997156226591fb0bec72835d6e42
melosomelo <mateusmelornrn@gmail.com>
2024-06-27 14:33:39 -0300

    add routers

--- noc/src/index.ts
+++ noc/src/index.ts
@@ -33,7 +33,8 @@ interface MoveAction {
 
 interface WaitAction {
   type: ActionType.Wait;
-  packetCulprit: number;
+  targetRouter: Coordinate;
+  targetRouterPort: Direction;
 }
 
 interface GiveUpAction {
@@ -63,29 +64,70 @@ class Packet {
   }
 }
 
+class Router {
+  blocked: boolean = false;
+  buffersCapacity: Record<Direction, number> = {
+    [Direction.East]: 0,
+    [Direction.West]: 0,
+    [Direction.North]: 0,
+    [Direction.South]: 0,
+  };
+}
+
 function main(config: Config) {
-  const blockedCells = new Set<string>(
-    config.blockedCells.map((cell) => `${cell.x},${cell.y}`)
+  const routers: Array<Array<Router>> = new Array(8).fill(
+    new Array(8).fill(new Router())
   );
   const packets = config.packets.map(
     (packet, i) =>
       new Packet(packet.origin, packet.destination, packet.payload, i)
+  );
+  packets.forEach(
+    (packet) =>
+      routers[packet.position.y][packet.position.x].buffersCapacity[
+        Direction.North
+      ]++
   );
   let activePackets = packets.filter(
     (packet) => packet.status === PacketStatus.Active
   );
   while (activePackets.length > 0) {
     for (let packet of activePackets) {
+      console.log(routers);
       const nextDirection = calculateNextDirection(packet);
       const nextPosition = move(packet.position, nextDirection);
+      const currentRouter = routers[packet.position.y][packet.position.x];
+      const nextRouter = routers[nextPosition.y][nextPosition.x];
       const { hasPacketInIt, packetCulprit } = cellHasPacketInIt(
         nextPosition,
         packets
       );
-      if (hasPacketInIt) {
+      if (canEnterRouter(nextRouter, oppositeDirection(nextDirection))) {
+        const packetLastMovement = getPacketLastMovement(packet);
+        if (packetLastMovement !== null) {
+          // decrement current router port counter
+          currentRouter.buffersCapacity[
+            oppositeDirection(packetLastMovement.direction)
+          ] -= 1;
+        }
+        // increment next router port counter
+        nextRouter.buffersCapacity[oppositeDirection(nextDirection)] += 1;
+        // update packet position and history
+        packet.position = nextPosition;
+        packet.history.push({
+          type: ActionType.Move,
+          direction: nextDirection,
+        });
+        packet.subsequentRoundsWaiting = 0;
+        if (equalCoordinates(packet.position, packet.destination)) {
+          packet.status = PacketStatus.Completed;
+        }
+      } else {
+        // wait
         packet.history.push({
           type: ActionType.Wait,
-          packetCulprit: packetCulprit!,
+          targetRouter: { x: nextPosition.x, y: nextPosition.y },
+          targetRouterPort: oppositeDirection(nextDirection),
         });
         packet.subsequentRoundsWaiting++;
         if (packet.subsequentRoundsWaiting === 10) {
@@ -93,15 +135,6 @@ function main(config: Config) {
             type: ActionType.GiveUp,
           });
           packet.status = PacketStatus.GivenUp;
-        }
-      } else {
-        packet.history.push({
-          type: ActionType.Move,
-          direction: nextDirection,
-        });
-        packet.position = nextPosition;
-        if (equalCoordinates(packet.position, packet.destination)) {
-          packet.status = PacketStatus.Completed;
         }
       }
     }
@@ -129,7 +162,7 @@ function main(config: Config) {
         pos = nextPos;
       } else if (action.type === ActionType.Wait) {
         console.log(
-          `\t - Waiting at (${pos.x},${pos.y}) because of packet ${action.packetCulprit}`
+          `\t - Waiting to enter router (${action.targetRouter.x},${action.targetRouter.y}) at port ${action.targetRouterPort}`
         );
       } else if (action.type === ActionType.GiveUp) {
         console.log(`\t - Gave up`);
@@ -162,6 +195,26 @@ function cellHasPacketInIt(cell: Coordinate, packets: Array<Packet>) {
     hasPacketInIt: packetCulprit !== undefined,
     packetCulprit: packetCulprit?.id,
   };
+}
+
+function canEnterRouter(router: Router, port: Direction) {
+  return router.buffersCapacity[port] < 4;
+}
+
+function oppositeDirection(direction: Direction) {
+  if (direction === Direction.South) return Direction.North;
+  if (direction === Direction.North) return Direction.South;
+  if (direction === Direction.East) return Direction.West;
+  return Direction.East;
+}
+
+function getPacketLastMovement(packet: Packet) {
+  for (let i = packet.history.length - 1; i >= 0; i--) {
+    if (packet.history[i].type === ActionType.Move) {
+      return packet.history[i] as MoveAction;
+    }
+  }
+  return null;
 }
 
 function move(position: Coordinate, direction: Direction): Coordinate {
